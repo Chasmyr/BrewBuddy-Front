5253fbbd4064cbf28f10f8e172925930db7f5b1f
Chasmyr <lestratgabin5886@gmail.com>
2025-06-12 20:00:53 +0200

    fix/call http to https

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
 
+if (window.location.protocol === 'http:') {
+  window.location.href = window.location.href.replace('http:', 'https:');
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
